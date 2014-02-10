6748425cac37d6897854f8f2e4f8e666774b43ca
Garret Handel <ghandel333@gmail.com>
2014-02-10 17:32:29 -0600

    Nearly complete.
    Now properly obtaining associated divisions and (mostly) departments for
    all courses. Haven't done a thorough check, but might not.

--- course_guide_xml_parser.py
+++ course_guide_xml_parser.py
@@ -26,7 +26,7 @@ file_in = open('div_dep.in', 'r')'''
 split_str = 'Chem\s\d+|Math\s.+|Grad\ss?t?o?r?|Cons\s.*|Biochem\s.*|AAE\s.*|[FJS][ro]/?\s.*|BSE\s.*|\(|Physics\s\d.*|Open\s.*| \
 Junior\s.*|College\slevel.*|Dy\sSci\s.*|An\s[^I].*|Intro\s.*|Entom\s.*|Food\sSci\s.*|Class\sCr.*|Stdt.*|Genetics\s\d.*| \
 Honors\scandidacy.*|Honors\sprogram.*|Hon\sprog\s.*|Civ\sEngr.*|L\sSc\sCom\s.*|Land\sArc\s.*|At\sleast\s.*|Journ\s.*|Microbio\s.*| \
-Variable\s.*|1st\s.*|None|Env\sTox\s.*|Not\sopen\s.*|Consent\s.*|Admission\s.*|Nutr\s.*|A?\s?\d\scr\s.*|Acct\s.*|Graduate\sstanding\s.*| \
+Variable\s.*|1st\s.*|None|Env\sTox\s.*|Not\sopen\s.*|Consent\s.*|Admission\s.*|Nutr\s.*|A?\s?\d\scr\s.*|Acct\s.*|(Graduate|Senior)\sstanding\s.*| \
 PhD\scand\s.*|Sophomore/?\ssta.*|Prerequisite\s.*|Stat\s.*|Comp\sSci/?\s?.*|Info\sSys\s.*|Spanish\s\d.*|Econ\s.*|Gen\sBus\s.*| \
 Admitted\s.*|Finance\s\d.*|O[TI]M\s.*|One\ssemester\s.*|RMI\s.*|Art\s\d.*|Art\sEd/\sCurric\s.*|Couns?\sPsy\s.*| \
 Successful\s.*|Adv\sreg\s.*|Curric\s\d.*|Tchg\s.*|Com\sDis\s.*|Dance\s\d.*|Intermediate/Advanced.*| \
@@ -37,23 +37,51 @@ Graded\son\s.*|Undergrad\scom\s.*|Varies\s.*|Communication\sArts\s250\sand.*|Com
 Students\sma?u?y?s?\s?.*|Honors\scand.*|BMI\s.*|E\sAsian\s.*|E\sA\sStds.*|Declared\s.*|Limited\s.*|A\slower\s.*| \
 Vet\sMed\s.*|Good\sacademic\s.*|DPH-2\s.*|PH-3\s.*|Phm\sSci\s.*|NUR\s.*|Nursing\smajor\s.*|Nurs\s\d.*|Nursing\s\d.*| \
 DPM-1\s.*|4?t?h?3?r?d?\syr\s.*|Pop\sHlth\s.*|MPH\sst\s.*|prof\sst\s.*|PA\scla.*|DPT\sst.*|Physiol\s\d.*|Neurosci\s\d.*| \
-Health\sprofessional\s.*|Med\sMicro\s.*|\d\ssem\s.*|Enrollment\sin\s.*|Statistics\s\d.*|Zool\s\d.*|Theatre\s\d.*| \
+Health\sprofessional\s.*|Med\sMicro\s.*|(\d|A)\ssem\s.*|Enrollment\sin\s.*|Statistics\s\d.*|Zool\s\d.*|Theatre\s\d.*| \
 Stats/.*|Math/.*|Span\s\d.*|Spanish\slang.*|Soc\swork.*|Scand\sSt\s\d.*|Portuguese\s\d.*|ILS\s\d.*|One\scrse\s.*| \
 Enroll\sin\s.*|Philos\s\d.*|Physcis\s\d.*|Am\sLit\s.*|Submission\s.*|ESL\sAss.*|Prerequisites\s.*|Grade\sof\s.*| \
-Com\sA\..*|Adv\s.*'
+Com\sA\..*|Adv\s.*|Quantitative\s.*'
+
+#division_search = '<[TH4]{2}\sid\=\"LinkTarget_\d{4}">\s?(College\sof\s[a-zA-Z ]+)\(.*\).*'
+division_search = '<(TH|H4)\sid\=\"LinkTarget_\d{4}\">((College\sof|School\sof|Air,)[a-zA-Z&;, ]+)\([a-zA-Z& ]+\)\s?([A-Z&, ]+)(\(\d\d\d\s\))?'
+#department_search = '<(TH|H4)\sid\=\"LinkTarget_\d{4}">\s?([A-Z&;\(\) ]+)</[TH4]{2}>'
+department_search = '<(TH|H4)\sid\=\"LinkTarget_\d{4}">\s?([A-Z&;\(\) ]+)</[TH4]{2}>([a-zA-Z&;, ]+\(.*\).*)?'
+course_search = '<[DHPT6]{1,2}>\s?(\d{3})\s([a-zA-Z0-9\.&;:,\-()/ ]+)</[DHPT6]{1,2}>'
+
+division = ''
+department =''
+course_title = ''
+course_number = ''
 
 for line in filehandle:
-    #temp = re.search(r'<[HPT6]{1,2}>\s?(\d\d\d\s[a-zA-Z.,\- ]+)[a-zA-Z0-9. ]+</[HPT6]{1,2}>', line)
-    temp = re.search(r'<H4\sid\=\"LinkTarget_\d{4}">\s?([a-zA-Z()&; ]+)\s?</H4>', line)
-    if temp is not None:
-        department = temp.group(1).replace('&amp;', 'AND').replace(',', '/')
+    # find the DEPARTMENT
+    line = line.replace('&amp;', '&')
+    # find the DIVISION
+    division_temp = re.search(r'%s' % (division_search), line)
+    if division_temp is not None:
+        division = division_temp.group(2).replace(',', '/')
+        if division_temp.group(4) is not None:
+            department = division_temp.group(4).replace(',', '/')
     else:
-        temp = re.search(r'<[DHPT6]{1,2}>\s?(\d{3})\s([a-zA-Z0-9\.&;:,\-()/ ]+)</[DHPT6]{1,2}>', line)
-        if temp is not None:
-            class_number = temp.group(1)
-            class_title = re.split(split_str, temp.group(2).replace('&amp;', 'AND').replace(',', '/'))[0]
-            if re.search(r'\d\d\d', class_title) is None:
-                udds.append('%s,%s,%s' % (department, class_number, class_title))
+        department_temp = re.search(r'%s' % (department_search), line)
+        if department_temp is not None:
+            department = department_temp.group(2).replace(',', '/')
+
+
+            # check to see if DEPARTMENT is stuck on end of DIVISION
+            '''department_temp = re.split('\)', division)
+            if len(department_temp) > 1:
+                temp = re.search(r'[A-Z ]+', department_temp[1])
+                if temp is not None and temp.group(0) != department:
+                    department = temp.group(0)
+                    division = '%s)' % department_temp[0]'''
+        else:
+            course_temp = re.search(r'%s' % (course_search), line)
+            if course_temp is not None:
+                course_number = course_temp.group(1)
+                course_title = re.split(split_str, course_temp.group(2).replace(',', '/'))[0]
+                if re.search(r'\d\d\d', course_title) is None:
+                    udds.append('%s,%s,%s,%s' % (division.rstrip(), department.rstrip(), course_number, course_title))
     '''else:
         temp = re.search(r'A\d{2}\s+\d{2}\s+([a-zA-Z ]+)\s+', line)
         if temp is not None:
@@ -67,7 +95,7 @@ for line in filehandle:
                 if unit not in udds[div][dept]:
                     udds[div][dept].append(unit)'''
 
-file_out.write('Division, Departent, Unit\n')
+file_out.write('Division, Departent, Course Number, Course Title\n')
 
 """for division in udds:
     for department in udds[division]:
