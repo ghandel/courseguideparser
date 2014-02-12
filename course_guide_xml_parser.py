4609c01e7f2953dda06e8be72e58f780fddaf82a
Garret Handel <ghandel333@gmail.com>
2014-02-11 18:16:57 -0600

    Added moar stufvz
    Now searching for and extracting course subject and instructor. Also
    added columns: Subject Number Title, Instructor, and Subject. Still need
    to check all outputs and clean up instructors and course titles.

--- course_guide_xml_parser.py
+++ course_guide_xml_parser.py
@@ -39,18 +39,22 @@ DPM-1\s.*|4?t?h?3?r?d?\syr\s.*|Pop\sHlth\s.*|MPH\sst\s.*|prof\sst\s.*|PA\scla.*|
 Health\sprofessional\s.*|Med\sMicro\s.*|(\d|A)\ssem\s.*|Enrollment\sin\s.*|Statistics\s\d.*|Zool\s\d.*|Theatre\s\d.*| \
 Stats/.*|Math/.*|Span\s\d.*|Spanish\slang.*|Soc\swork.*|Scand\sSt\s\d.*|Portuguese\s\d.*|ILS\s\d.*|One\scrse\s.*| \
 Enroll\sin\s.*|Philos\s\d.*|Physcis\s\d.*|Am\sLit\s.*|Submission\s.*|ESL\sAss.*|Prerequisites\s.*|Grade\sof\s.*| \
-Com\sA\..*|Adv\s.*|Quantitative\s.*'
+Com\sA\..*|Adv\s.*|Quantitative\s.*|Freshmen,\s.*|(Fr|So|Jr|Sr),\s.*'
 
 # Define search strings for finding division, department, and course number and title
 division_search = '<(TH|H4)\sid\=\"LinkTarget_\d{4}\">((College\sof|School\sof|Air,)[a-zA-Z&;, ]+)\([a-zA-Z& ]+\)\s?(AIR\sFORCE\sAEROSPACE\sSTUDIES\s|AFRICAN\sLANGUAGES\s&LITERATURE\s|COMPARATIVE\sBIOSCIENCES\s)?((A\sF\sAERO\s|AFRICAN\s|COMP\sBIO\s)\(\d\d\d\s\))?'
-# This was the old, robust division search before the exceptions above '<(TH|H4)\sid\=\"LinkTarget_\d{4}\">((College\sof|School\sof|Air,)[a-zA-Z&;, ]+)\([a-zA-Z& ]+\)\s?([A-Z&, ]+)(\(\d\d\d\s\))?'
+# This was the old, robust division search before the exceptions above =============> '<(TH|H4)\sid\=\"LinkTarget_\d{4}\">((College\sof|School\sof|Air,)[a-zA-Z&;, ]+)\([a-zA-Z& ]+\)\s?([A-Z&, ]+)(\(\d\d\d\s\))?'
 department_search = '<(TH|H4)\sid\=\"LinkTarget_\d{4}">\s?([A-Z&;\(\) ]+)</[TH4]{2}>([a-zA-Z&;, ]+\(.*\).*)?'
-
+subject_search = '<(TH|P)>(([A-Z ]+)\s\(\d\d\d\s\))\s.*'
+instructor_search = '<TD>Instructor\s([a-zA-z,\. ]*)?</TD>'
 course_search = '<[DHPT6]{1,2}>\s?(\d{3})\s([a-zA-Z0-9\.&;:,\-()/ ]+)</[DHPT6]{1,2}>'
 
 # Initialize variables to avoid uninitialized variable errors
 division = ''
 department =''
+instructor = ''
+subject = ''
+subject_number_title = ''
 course_title = ''
 course_number = ''
 courses = []
@@ -61,24 +65,36 @@ for line in filehandle:
     # find the DIVISION
     division_temp = re.search(r'%s' % (division_search), line)
     if division_temp is not None:
-        division = division_temp.group(2).replace(',', '/')
+        division = division_temp.group(2)
         # Check for DEPARTMENT included on same line as division (only a couple cases of this)
         if division_temp.group(4) is not None:
-            department = division_temp.group(4).replace(',', '/')
+            department = division_temp.group(4)
+            subject = division_temp.group(6)
     else:
         # find the DEPARTMENT
         department_temp = re.search(r'%s' % (department_search), line)
         if department_temp is not None:
-            department = department_temp.group(2).replace(',', '/')
+            department = department_temp.group(2)
         else:
-            # find COURSE TITLE and COURSE NUMBER
-            course_temp = re.search(r'%s' % (course_search), line)
-            if course_temp is not None:
-                course_number = course_temp.group(1)
-                course_title = re.split(split_str, course_temp.group(2).replace(',', '/'))[0]
-                # Extract COURSE NUMBER and add COURSE INFO to the array
-                if re.search(r'\d\d\d', course_title) is None:
-                    courses.append('%s,%s,%s,%s' % (division.rstrip(), department.rstrip(), course_number, course_title))
+            # find SUBJECT
+            subject_temp = re.search(r'%s' % (subject_search), line)
+            if subject_temp is not None:
+                subject = subject_temp.group(3)
+            else:
+                # find INSTRUCTOR
+                instructor_temp = re.search(r'%s' % (instructor_search), line)
+                if instructor_temp is not None:
+                    instructor = instructor_temp.group(1)
+                else:
+                    # find COURSE TITLE and COURSE NUMBER
+                    course_temp = re.search(r'%s' % (course_search), line)
+                    if course_temp is not None:
+                        course_number = course_temp.group(1)
+                        course_title = re.split(split_str, course_temp.group(2))[0]
+                        # Extract COURSE NUMBER and add COURSE INFO to the array
+                        if re.search(r'\d\d\d', course_title) is None:
+                            subject_number_title = ('%s %s %s' % (subject, course_number, course_title))
+                            courses.append('\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\'' % (subject_number_title, instructor, division.rstrip(), department.rstrip(), subject, course_number, course_title))
 
 # Initialize CSV with headers
 file_out.write('Subject Number Title,Instructor,Division,Department,Subject,Course Number,Course Title\n')
